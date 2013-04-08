37dd0bb9a7b982a2e47f3f52b5dda6d4cc5df2a8
Lance Hepler <nlhepler@gmail.com>
2013-04-08 12:32:36 -0700

    use python3, not 3.2 specifically

--- bin/SimulateRandom.py
+++ bin/SimulateRandom.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3.2
+#!/usr/bin/env python3
 
 import csv, argparse, sys, datetime, time, random, os, math
 #from scipy import stats
@@ -20,14 +20,14 @@ def describe_network (network):
 def crude_density_estimate (vector, min, max, step, value):
     dim = math.ceil((max-min)/step)
     density = [0.5 for k in range (dim)]
-    
+
     for k in vector:
         bin = int ((k - min) // step)
         density[bin] += 1
 
     norm = sum (density)
     density = [k / norm  for k in density]
-    
+
     return density [int ((value - min) // step)]
 
 print ("\t".join(["Size","Bias","MeanEdges","MeanNodes","MeanClusters","P"]))
@@ -42,7 +42,7 @@ for int_bias in range (50,51,5):
         edges    = []
         clusters = []
         nodes    = []
-        
+
         for rep in range (replicates):
             sfn = transmission_network ()
             sfn = sfn.create_a_pref_attachment_network (size).sample_from_network (N,node_sampling_bias=bias)
@@ -53,7 +53,7 @@ for int_bias in range (50,51,5):
             nodes += [network_stats['nodes']]
             clusters += [len (sfn_clusters)]
             #print (edges)
-        
+
         prob = math.log(crude_density_estimate(edges,0,2000,10,expected_stats['edges'])*crude_density_estimate(nodes,0,650,5,expected_stats['nodes'])*crude_density_estimate(clusters,0,200,5,expected_stats['clusters']));
-        
+
         print ("\t".join([str(k) for k in [size,bias,sum(edges)/replicates,sum(nodes)/replicates,sum(clusters)/replicates, prob]]))
